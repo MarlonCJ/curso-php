89b5738c8a96c3a9246a19dd5097dd58f18f27fa
MarlonCJ <marlon.edinsoncj@gmail.com>
2026-01-27 05:29:38 -0500

    to do paso 1

--- proyectos/todo-php/index.php
+++ proyectos/todo-php/index.php
@@ -2,6 +2,14 @@
 
 session_start();
 
+/*
+ *Si el array de tareas no existe en la sesion
+ *lo creamos por primera vez 
+ */
+
+if(!isset($_SESSION['task'])){
+    $_SESSION['task'] = [];
+}
 ?>
 
 <!DOCTYPE html>
@@ -14,6 +22,13 @@ session_start();
 <body>
     <h1>Mi primera app en PHP</h1>
 
-    <p>La sesion esta activa</p>
+    <p>Contenido actual de la sesion:</p>
+
+<pre>
+    <?php  
+        print_r($_SESSION['task']);
+    ?>
+</pre>
+
 </body>
 </html>
