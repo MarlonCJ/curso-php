757fb1e700749e943ea171d0c9e55f9a95fd5c5a
MarlonCJ <marlon.edinsoncj@gmail.com>
2026-01-27 05:23:03 -0500

    to do : paso 0

---
+++ proyectos/todo-php/index.php
@@ -0,0 +1,19 @@
+<?php
+
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TO-DO PHP</title>
+</head>
+<body>
+    <h1>Mi primera app en PHP</h1>
+
+    <p>La sesion esta activa</p>
+</body>
+</html>
