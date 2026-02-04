ba150156aea99e027949499cae65b01de7a9550c
MarlonCJ <marlon.edinsoncj@gmail.com>
2026-02-04 06:16:54 -0500

    bloque 3: listado de clientes

---
+++ proyectos/bloque-3/index.php
@@ -0,0 +1,85 @@
+<?php
+
+// Listado de Usuarios
+
+// Nombre: Juan Pérez
+// Email: juan@email.com
+// Edad: 28
+// Estado: Activo
+// ---------------------
+
+// Nombre: Ana Gómez
+// Email: ana@email.com
+// Edad: 17
+// Estado: Inactivo
+// ---------------------
+
+// Nombre: Carlos Ruiz
+// Email: carlos@email.com
+// Edad: 35
+// Estado: Activo
+// ---------------------
+
+$usuarios = [
+    [
+        'nombre' => 'Juan Pérez',
+        'email' => 'juan@email.com',
+        'edad' => 28,
+        'estado' => 'Activo',
+    ],
+    [
+        'nombre' => 'Ana Gómez',
+        'email' => 'ana@email.com',
+        'edad' => 17,
+        'estado' => 'Inactivo',
+    ],
+    [
+        'nombre' => 'Carlos Ruiz',
+        'email' => 'carlos@email.com',
+        'edad' => 35,
+        'estado' => 'Activo',
+    ]
+];
+
+
+// SE PUEDE PERO NO TIENES CONTROL DE LAS SALIDAD DE LOS DATOS
+// foreach($usuarios as $clave => $valor){
+//     foreach($valor as $usuario => $val){
+//         echo $usuario ." : " . $val ."<br>";
+//     }
+//     echo "<br>";
+// }
+
+// MEJOR FORMA
+
+
+// foreach($usuarios as $usuario){
+//     echo 'Nombre: '. $usuario['nombre'] . '<br>';
+//     echo 'Nombre: '. $usuario['email'] . '<br>';
+//     echo 'Nombre: '. $usuario['edad'] . '<br>';
+//     echo 'Nombre: '. $usuario['estado'] . '<br>';
+//     echo '----------------------------<br>';
+// }
+
+
+// ===SALIDAS===
+
+echo '<!DOCTYPE html>';
+echo '<head>';
+echo '  <meta lang="es">';
+echo '  <meta charset="UTF-8">';
+echo '  <title>Listado de Usuarios</title>';
+echo '</head>';
+echo '<body>';
+echo '<h1>Listado de Usuarios</h1>';
+foreach($usuarios as $usuario){
+    echo 'Nombre: '. $usuario['nombre'] . '<br>';
+    echo 'Nombre: '. $usuario['email'] . '<br>';
+    echo 'Nombre: '. $usuario['edad'] . '<br>';
+    echo 'Nombre: '. $usuario['estado'] . '<br>';
+    echo '----------------------------<br>';
+}
+echo '</body>';
+echo '</html>';
+
+
